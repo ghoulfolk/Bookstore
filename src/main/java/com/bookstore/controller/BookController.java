61fe5ce8df6923da0312c10a9699d03241474f71
Tatu Mäkelä <tatu.makela@solinor.com>
2017-03-02 14:55:06 +0200

    added /api endpoint to work on by having the application.properties file include the spring setting

--- src/main/java/com/bookstore/controller/BookController.java
+++ src/main/java/com/bookstore/controller/BookController.java
@@ -1,6 +1,9 @@
 package com.bookstore.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.rest.webmvc.BasePathAwareController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,21 +11,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 import com.bookstore.model.Book;
 import com.bookstore.model.BookRepository;
 
 @Controller
-//@ResponseBody
+//@BasePathAwareController
 public class BookController {
 
 	@Autowired
 	BookRepository repository;
 
-	@RequestMapping("/")
-	public String index (){
-		return "This is the index-page";	
-	}
+//	@RequestMapping("/")
+//	public String index (){
+//		return "This is the index-page";	
+//	}
 
+	// RESTful service to get all students
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) repository.findAll();
+    }    
+
+	// RESTful service to get student by id
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Book findBookRest(@PathVariable("id") Long bookId) {	
+    	return repository.findOne(bookId);
+    }   
+    
 	@RequestMapping(value="/booklist")
 	public String bookList(Model model) {	
 		model.addAttribute("books", repository.findAll());
--- src/main/java/com/bookstore/model/Book.java
+++ src/main/java/com/bookstore/model/Book.java
@@ -1,16 +1,14 @@
 package com.bookstore.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter @Setter
+@Data
 @Entity
 @Table(name = "book")
 public class Book {
